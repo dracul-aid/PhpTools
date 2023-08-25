0e6e366dc0b1b7b52b4010d7629b06d3301ebc4b
Konstantin Marataev <29762625+dracul-aid@users.noreply.github.com>
2023-08-25 11:12:17 +0400

    Добавлен абрстрактный класс для всех тестов
    Добавлен \DraculAid\PhpTools\tests\AbstractProjectTestCase наследующий от \PHPUnit\Framework\TestCase
    
    Класс расширяет тест-кейсы, добавляя в них сравнение таймштампов

---
+++ tests/AbstractProjectTestCase.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace DraculAid\PhpTools\tests;
+
+use DraculAid\PhpTools\DateTime\Dictionary\DateTimeFormats;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Расширение тест-класса под нужды библиотеки PhpTools
+ */
+class AbstractProjectTestCase extends TestCase
+{
+    /**
+     * Сравнение таймштампов
+     *
+     * @param   mixed    $expected    Ожидаемое значение (верное значение)
+     * @param   mixed    $actual      Проверяемое значение (что получилось в результате тестового вызова)
+     * @param   string   $message     Сообщение, выводится при провале теста (Дополняет сообщение генерируемое функцией)
+     * @return  void
+     *
+     * @todo PHP8 типизация аргументов функции
+     */
+    public static function assertTimestamp($expected, $actual, string $message = ''): void
+    {
+        if (!is_int($expected)) $message = ($message ? "{$message} " : '') . '$expected can be int, but it is a ' . gettype($expected);
+        if (!is_int($expected)) $message = ($message ? "{$message} " : '') . '$actual can be int, but it is a ' . gettype($expected);
+
+        if ($expected != $actual) $message = ($message ? "{$message} " : '')
+            . '$expected = ' . date(DateTimeFormats::SQL_DATETIME, $expected)
+            . ', but $actual = ' . date(DateTimeFormats::SQL_DATETIME, $actual);
+
+        self::assertEquals($expected, $actual, $message);
+    }
+}

63d78d416b05a2c46263f66249f1144a9fc2f4c3
Konstantin Marataev <29762625+dracul-aid@users.noreply.github.com>
2023-08-26 11:36:57 +0400

    Часовой пояс для теста UTC
    Для облегчения тестирования функций для работы с датой-временем установлен часовой пояс UTC (часовые пояса с летним/зимнем временем сильно усложняют тестирование функций даты-времени, их доработка под корректную работу в таких часовых поясах будет произведена в будущем)

--- tests/run.php
+++ tests/run.php
@@ -18,6 +18,18 @@ if ($phpUnitPath)
     $phpUnitCodeExecutor = explode("\n", file_get_contents($phpUnitPath));
     unset($phpUnitCodeExecutor[0], $phpUnitCodeExecutor[1]);
 
+    /**
+     * Установка UTC часового пояса
+     * функции по работе с временем, оттестированы для работы в UTC
+     *
+     * @link https://en.wikipedia.org/wiki/List_of_tz_database_time_zones Список часовых поясов
+     *
+     * @todo При доработке функций для работы с датой-временем, под часовые пояса, необходимо будет убрать эту настройку
+     *       Необходимо будет проверять все функции в разных часовых поясах, а также в летнем и зимне времени
+     */
+    date_default_timezone_set('UTC'); // UTC (0-вой часовой пояс)
+    //date_default_timezone_set('Europe/Moscow'); // Москва (+3-вой часовой пояс)
+
     eval(implode($phpUnitCodeExecutor));
 }
 else

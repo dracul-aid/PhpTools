3337f2cfd307e7b3167103707b6dc32eb6f320f6
Konstantin Marataev <29762625+dracul-aid@users.noreply.github.com>
2023-10-29 23:11:01 +0400

    Добавлен функционал, облегчающий вызов функций
    Добавлен класс DraculAid\PhpTools\Functions\CallFunctionHelper - облегчающий вызов функций. Позволяет вызывать конструкции языка PHP как функции, а также функции, с попыткой передать им большее кол-во аргументов, чем они поддерживают

---
+++ src/Functions/CallFunctionHelper.php
@@ -0,0 +1,180 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of PhpTools - https://github.com/dracul-aid/PhpTools
+ *
+ * (c) Konstantin Marataev <dracul.aid@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DraculAid\PhpTools\Functions;
+
+/**
+ * Функционал, облегчающий вызов функций
+ *
+ * 1. Позволяет вызывать языковые конструкции, как функции
+ * 2. Позволяет вызывать функции, передавая в них больше аргументов, чем готова принять функция
+ *
+ * Оглавление
+ * <br>{@see CallFunctionHelper::STRUCTURES} Список структур, которые можно вызвать через {@see CallFunctionHelper::exe()}
+ * <br>{@see CallFunctionHelper::exe()} Вызовет функцию или языковую конструкцию
+ * <br>{@see CallFunctionHelper::isStructures()} Проверяет переданное значение, является оно языковой конструкцией или нет
+ * <br>{@see CallFunctionHelper::isClassCallable()} Проверит, "вызываемое" является вызовом метода или нет
+ * <br>{@see CallFunctionHelper::isCallable()} Проверяет, может ли переданная строка (иное `callable`) быть вызвано как "функция"
+ * <br>{@see CallFunctionHelper::getReflectionForCallable()} Вернет рефлексию для "Вызываемого", определив чем оно является, методом или функцией
+ */
+final class CallFunctionHelper
+{
+    /**
+     * Список структур, которые можно вызвать через {@see CallFunctionHelper::exe()}
+     */
+    public const STRUCTURES = [
+        'isset', /** {@see isset()} */
+        'empty', /** {@see empty()} */
+        'require', /** {@see require()} */
+        'require_once', /** {@see require_once()} */
+        'include', /** {@see include()} */
+        'include_once', /** {@see include_once()} */
+        'echo', /** {@see echo()} */
+        'print', /** {@see print()} */
+        'eval', /** {@see eval()} */
+    ];
+
+    /**
+     * Вызовет функцию или языковую конструкцию (корректно отработав случаи, в которых аргументов будет больше аргументов функции)
+     *
+     * (!) В {@see isset()} передается только 1 аргумент.
+     *
+     * @param   string|callable       $function     Функция или языковая конструкция
+     * @param   mixed              ...$arguments    Передаваемые аргументы
+     *
+     * @return  mixed
+     *
+     * @throws  \LogicException       Если $function не может быть вызвана
+     * @throws  \ReflectionException  В случае провала получения рефлексии для функции или метода
+     *
+     * @todo PHP8 аргументы и ответ функции
+     * @todo PHP8 Реализовать - Если нужно передать все аргументы, воспользуйтесь 'isset_list'
+     * @todo Реализовать - больше тестов для различных конструкций языка
+     * @todo Реализовать - тесты для вызова методов классов
+     */
+    public static function exe($function, ... $arguments)
+    {
+        if (is_callable($function)) return self::exeCallable($function, $arguments);
+
+        if ($function === 'isset') return isset($arguments[0]);
+        elseif ($function === 'empty') return empty($arguments[0]);
+        elseif ($function === 'require') return require($arguments[0]);
+        elseif ($function === 'require_once') return require_once($arguments[0]);
+        elseif ($function === 'include') return include($arguments[0]);
+        elseif ($function === 'include_once') return include_once($arguments[0]);
+        elseif ($function === 'eval') return eval($arguments[0]);
+        else throw new \LogicException("{$function} is not callable");
+    }
+
+    /**
+     * Проверяет переданное значение, является оно языковой конструкцией или нет
+     *
+     * (!) Вернет TRUE только для языковых конструкций, которые могут быть вызваны в {@see CallFunctionHelper::exe()}
+     *     (т.е. конструкции, которые часто воспринимаются, как функции)
+     *
+     * @param   string   $testValue
+     *
+     * @return  bool
+     */
+    public static function isStructures(string $testValue): bool
+    {
+        return $testValue === 'isset' /** {@see isset()} */
+            || $testValue === 'empty' /** {@see empty()} */
+            || $testValue === 'require' /** {@see require()} */
+            || $testValue === 'require_once' /** {@see require_once()} */
+            || $testValue === 'include' /** {@see include()} */
+            || $testValue === 'include_once' /** {@see include_once()} */
+            || $testValue === 'echo' /** {@see echo()} */
+            || $testValue === 'print' /** {@see print()} */
+            || $testValue === 'eval'; /** {@see eval()} */
+    }
+
+    /**
+     * Проверяет, может ли переданная строка (иное `callable`) быть вызвано как "функция" в {@see CallFunctionHelper::exe()}
+     *
+     * @param   string|callable   $function
+     *
+     * @return bool
+     *
+     * @todo PHP8 аргументы функции
+     */
+    public static function isCallable($function): bool
+    {
+        return is_callable($function) || self::isStructures($function);
+    }
+
+    /**
+     * Проверит, "вызываемое" является вызовом метода или нет
+     *
+     * @param   callable   $function      "Вызываемое" для проверки
+     *
+     * @return bool
+     */
+    public static function isClassCallable(callable $function): bool
+    {
+        return is_array($function) || (is_string($function) && strpos($function, '::') > 1);
+    }
+
+    /**
+     * Вернет рефлексию для "Вызываемого", определив чем оно является, методом или функцией
+     *
+     * (!) Конструкции языка (вроде `isset()`) не являются `callable` и для них невозможно получить рефлексию
+     *
+     * @param   callable   $function
+     *
+     * @return  \ReflectionFunctionAbstract
+     *
+     * @throws  \ReflectionException  В случае провала получения рефлексии для функции
+     */
+    public static function getReflectionForCallable(callable $function): \ReflectionFunctionAbstract
+    {
+        if (self::isClassCallable($function))
+        {
+            if (is_array($function)) return new \ReflectionMethod($function[0], $function[1]);
+            else return new \ReflectionMethod($function);
+        }
+
+        return new \ReflectionFunction($function);
+    }
+
+    /**
+     * Вызовет функцию, корректно отработав случаи, что передано больше аргументов, чем готова принять функция
+     *
+     * @param   callable   $function    Функция
+     * @param   array      $arguments   Аргументы
+     *
+     * @return  mixed
+     *
+     * @throws  \ReflectionException  В случае провала получения рефлексии для функции
+     */
+    private static function exeCallable(callable $function, array $arguments)
+    {
+        if (count($arguments) === 0) return call_user_func($function);
+        elseif (count($arguments) === 1) return call_user_func($function, $arguments[0]);
+
+        // * * *
+
+        $functionReflection = self::getReflectionForCallable($function);
+
+        // если аргументов передано больше, чем готова принять функция - урежем кол-во аргументов
+        if ($functionReflection->getNumberOfParameters() < count($arguments))
+        {
+            return call_user_func_array(
+                $function,
+                array_slice($arguments, 0, $functionReflection->getNumberOfParameters())
+            );
+        }
+        else
+        {
+            return call_user_func_array($function, $arguments);
+        }
+    }
+}
---
+++ tests/Functions/CallFunctionHelperTest.php
@@ -0,0 +1,101 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of PhpTools - https://github.com/dracul-aid/PhpTools
+ *
+ * (c) Konstantin Marataev <dracul.aid@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DraculAid\PhpTools\tests\Functions;
+
+use DraculAid\PhpTools\Functions\CallFunctionHelper;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test for {@see CallFunctionHelper}
+ *
+ * @run php tests/run.php tests/Functions/CallFunctionHelperTest.php
+ */
+class CallFunctionHelperTest extends TestCase
+{
+    /**
+     * Test for {@see CallFunctionHelper::STRUCTURES}
+     * Test for {@see CallFunctionHelper::isStructures()}
+     * Test for {@see CallFunctionHelper::isCallable()}
+     *
+     * @return void
+     */
+    public function testIsStructures(): void
+    {
+        foreach (CallFunctionHelper::STRUCTURES as $name)
+        {
+            self::assertTrue(CallFunctionHelper::isStructures($name), "isStructures: Error for {$name}");
+            self::assertTrue(CallFunctionHelper::isCallable($name), "isCallable: Error for {$name}");
+        }
+
+        self::assertFalse(CallFunctionHelper::isStructures('is_int'));
+        self::assertTrue(CallFunctionHelper::isCallable('is_int'));
+        self::assertFalse(CallFunctionHelper::isStructures(CallFunctionHelper::class . '::isStructures'));
+        self::assertTrue(CallFunctionHelper::isCallable(CallFunctionHelper::class . '::isStructures'));
+
+        self::assertTrue(CallFunctionHelper::isCallable([CallFunctionHelper::class, 'isStructures']));
+        self::assertTrue(CallFunctionHelper::isCallable([$this, 'testIsStructures']));
+    }
+
+    /**
+     * Test for {@see CallFunctionHelper::exe()}
+     * Test for {@see CallFunctionHelper::exeCallable()}
+     * Test for {@see CallFunctionHelper::isClassCallable()} in {@see CallFunctionHelper::exeCallable()}
+     * Test for {@see CallFunctionHelper::getReflectionForCallable()} in {@see CallFunctionHelper::exeCallable()}
+     *
+     * @return void
+     */
+    public function testExe(): void
+    {
+        // isset()
+        $t = null;
+        self::assertFalse(CallFunctionHelper::exe('isset', $t));
+        $t = false;
+        self::assertTrue(CallFunctionHelper::exe('isset', $t));
+        $t = false; $a = null;
+        self::assertTrue(CallFunctionHelper::exe('isset', $t, $a));
+
+        // empty()
+        $t = null; $a = null;
+        self::assertTrue(CallFunctionHelper::exe('empty', $t, $a));
+        $t = false;
+        self::assertTrue(CallFunctionHelper::exe('empty', $t, $a));
+        $t = '123';
+        self::assertFalse(CallFunctionHelper::exe('empty', $t, $a));
+
+        // * * * Вызов функций
+
+        $t = 'XXX';
+        self::assertFalse(CallFunctionHelper::exe('is_int', $t));
+        $t = 0;
+        self::assertTrue(CallFunctionHelper::exe('is_int', $t));
+        $t = 1; $a = 'XYZ';
+        self::assertTrue(CallFunctionHelper::exe('is_int', $t, $a));
+
+        // вызов методов
+        $testObject = $this->getTestObject();
+        self::assertEquals(3, CallFunctionHelper::exe([$testObject, 'f2'], 5, 2));
+    }
+
+    private function getTestObject(): object
+    {
+        return new class() {
+            public static function f1(int $a, int $b): int
+            {
+                return $a + $b;
+            }
+            public function f2(int $a, int $b): int
+            {
+                return $a - $b;
+            }
+        };
+    }
+}

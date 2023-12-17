14e314bda0add20a6d2eed78b8e42719e30419d5
Konstantin Marataev <29762625+dracul-aid@users.noreply.github.com>
2023-12-17 15:22:42 +0400

    Базовый класс для облегчения работы с PhpUnit

---
+++ src/TestTools/PhpUnit/PhpUnitExtendTestCase.php
@@ -0,0 +1,67 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of PhpTools - https://github.com/dracul-aid/PhpTools
+ *
+ * (c) Konstantin Marataev <dracul.aid@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DraculAid\PhpTools\TestTools\PhpUnit;
+
+use DraculAid\PhpTools\Classes\ClassNotPublicManager;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Расширяет стандартный тест-класс PhpUnit
+ *
+ * Оглавление:
+ * <br>{@see PhpUnitExtendTestCase::assertPropertyEquals()}  Проводит проверку для указанного свойства
+ * <br>{@see PhpUnitExtendTestCase::assertMethodEquals()} Проводит проверку результатов работы метода
+ */
+class PhpUnitExtendTestCase extends TestCase
+{
+    /**
+     * Проводит проверку для указанного свойства (в том числе и не публичного)
+     *
+     * @param   string|object   $classOrObject   Класс (для статического свойства) или объект
+     * @param   string          $property        Имя свойства
+     * @param   mixed           $equalValue      Ожидаемое значение свойства
+     * @param   string          $message         Дополнительное сообщение, будет сгенерировано в случае возникновения ошибки
+     *
+     * @return void
+     *
+     * @todo PHP8 Типизация аргументов
+     */
+    public static function assertPropertyEquals($classOrObject, string $property, $equalValue, string $message = ''): void
+    {
+        self::assertEquals(
+            ClassNotPublicManager::readProperty($classOrObject, $property),
+            $equalValue,
+            $message
+        );
+    }
+
+    /**
+     * Проводит проверку результатов работы метода (в том числе и не публичного)
+     *
+     * @param   array    $methodAsArray   Вызываемый метод в формате массива [$objectOrClass, $method]
+     * @param   array    $arguments       Массив аргументов
+     * @param   mixed    $equalValue      Ожидаемое значение свойства
+     * @param   string   $message         Дополнительное сообщение, будет сгенерировано в случае возникновения ошибки
+     *
+     * @return void
+     *
+     * @todo PHP8 Типизация аргументов
+     */
+    public static function assertMethodEquals(array $methodAsArray, array $arguments, $equalValue, string $message = ''): void
+    {
+        self::assertEquals(
+            ClassNotPublicManager::callMethod($methodAsArray, $arguments),
+            $equalValue,
+            $message
+        );
+    }
+}

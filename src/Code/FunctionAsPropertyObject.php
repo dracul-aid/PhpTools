61104e9821da15dfec68cc00c2254b06aa55d074
Konstantin Marataev <29762625+dracul-aid@users.noreply.github.com>
2024-01-05 12:45:38 +0400

    Добавлен функционал, для использования функций, в качестве методов классов
    Code\FunctionAsPropertyObject - Позволяет использовать в качестве методов классов любые функции (включая методы классов) и языковые конструкции PHP

---
+++ src/Code/FunctionAsPropertyObject.php
@@ -0,0 +1,162 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of PhpTools - https://github.com/dracul-aid/PhpTools
+ *
+ * (c) Konstantin Marataev <dracul.aid@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DraculAid\PhpTools\Code;
+
+/**
+ * Позволяет использовать функцию в качестве свойства классов
+ *
+ * Позволяет типизировать свойства классов, используя их для хранения анонимных функций ({@see \Closure}), обычных функций,
+ * методов классов, а также языковых конструкций PHP
+ *
+ * Оглавление:
+ * <br>{@see FunctionAsPropertyObject::getOrCreate()} Вернет (если надо создаст) объект, хранящий функцию
+ * <br>{@see self::setFunction()} Заменит установленную функцию
+ * <br>{@see self::call()} Произведет вызов
+ * <br>{@see self::callSafe()} Произведет защищенный вызов
+ * <br>{@see self::getFunction()} Вернет установленную функцию
+ */
+class FunctionAsPropertyObject
+{
+    /**
+     * Хранимая функция является "функцией" или нет
+     *
+     * Возможные варианты
+     * <br>FALSE: это не функция, а языковая конструкция PHP
+     * <br>TRUE: это функция (точнее один из вариантов вызываемого, см PHP псевдотип `callable`)
+     */
+    protected bool $is_callable = false;
+
+    /**
+     * Будет произведен "защищенный вызов"
+     * (если при вызове передано больше аргументов, чем может принять функция, лишние аргументы будут отброшены)
+     */
+    protected bool $safeCall = true;
+
+    /**
+     * Хранимая функция
+     *
+     * @var string|callable
+     *
+     * @todo PHP8 типизация
+     */
+    protected $function;
+
+    /**
+     * Создаст объект, хранящий функцию
+     *
+     * @param   string|callable   $function   Функция или языковая конструкция
+     * @param   bool              $safeCall   Будет произведен "защищенный вызов" (если при вызове передано больше аргументов,
+     *                                        чем может принять функция, лишние аргументы будут отброшены)
+     *
+     * @todo PHP8 типизация аргументов функции
+     */
+    public function __construct($function, bool $safeCall = true)
+    {
+        $this->setFunction($function, $safeCall);
+    }
+
+    /**
+     * Вернет объект хранящий функцию:
+     * <br>Если передана функция или языковая конструкция - вернет объект хранящий функцию
+     * <br>Если передан объект хранящий функцию, вернет именно его
+     *
+     * @param   string|callable|FunctionAsPropertyObject   $functionOrObject
+     *
+     * @return  static
+     *
+     * @todo PHP8 типизация аргументов функции, также часть проверок в коде станет ненужной
+     */
+    public static function getOrCreate($functionOrObject): self
+    {
+        if ($functionOrObject instanceof self) return $functionOrObject;
+
+        // @todo PHP8 проверка теряет смысл
+        if (is_callable($functionOrObject) || is_string($functionOrObject)) return new static($functionOrObject);
+
+        // @todo PHP8 удаляем, так как никогда не будет выполняться
+        throw new \TypeError('$functionOrObject can be string|callable|FunctionAsPropertyObject');
+    }
+
+    /**
+     * Создаст объект, хранящий функцию
+     *
+     * @param   string|callable   $function   Функция или языковая конструкция
+     * @param   bool              $safeCall   Будет произведен "защищенный вызов" (если при вызове передано больше аргументов,
+     *                                        чем может принять функция, лишние аргументы будут отброшены)
+     *
+     * @todo PHP8 типизация аргументов функции
+     */
+    public function setFunction($function, bool $safeCall = true): self
+    {
+        $this->is_callable = is_callable($function);
+        $this->safeCall = $safeCall;
+        $this->function = $function;
+
+        return $this;
+    }
+
+    /**
+     * Вызовет функцию
+     *
+     * @param   mixed   ...$arguments   Список аргументов
+     *
+     * @return  mixed
+     *
+     * @todo PHP8 типизация аргументов и ответа функции
+     */
+    public function call(...$arguments)
+    {
+        if ($this->safeCall || !$this->is_callable) return CallFunctionHelper::exe($this->function, ... $arguments);
+        else return call_user_func_array($this->function, $arguments);
+    }
+
+    /**
+     * Вызовет функцию в защищенном режиме
+     * (если при вызове передано больше аргументов, чем может принять функция, лишние аргументы будут отброшены)
+     *
+     * @param   mixed   ...$arguments   Список аргументов
+     *
+     * @return  mixed
+     *
+     * @todo PHP8 типизация аргументов и ответа функции
+     */
+    public function callSafe(...$arguments)
+    {
+        return CallFunctionHelper::exe($this->function, ... $arguments);
+    }
+
+    /**
+     * Вернет установленную функцию
+     *
+     * @return string|callable
+     *
+     * @todo PHP8 типизация ответа функции
+     */
+    public function getFunction()
+    {
+        return $this->function;
+    }
+
+    /**
+     * Вызов объекта, как функции
+     *
+     * @param   mixed   ...$arguments   Список аргументов
+     *
+     * @return  mixed
+     *
+     * @todo PHP8 типизация аргументов и ответа функции
+     */
+    public function __invoke(...$arguments)
+    {
+        return $this->call(...$arguments);
+    }
+}
---
+++ tests/Code/FunctionAsPropertyObjectTest.php
@@ -0,0 +1,83 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of PhpTools - https://github.com/dracul-aid/PhpTools
+ *
+ * (c) Konstantin Marataev <dracul.aid@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DraculAid\PhpTools\tests\Code;
+
+use DraculAid\PhpTools\Code\FunctionAsPropertyObject;
+use DraculAid\PhpTools\ExceptionTools\ExceptionTools;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test for {@coversDefaultClass FunctionAsPropertyObject}
+ *
+ * @run php tests/run.php tests/Code/FunctionAsPropertyObjectTest.php
+ */
+class FunctionAsPropertyObjectTest extends TestCase
+{
+    /**
+     * Test for {@covers FunctionAsPropertyObject::__construct()}
+     * Test for {@covers FunctionAsPropertyObject::setFunction()}
+     * Test for {@covers FunctionAsPropertyObject::call()}
+     * Test for {@covers FunctionAsPropertyObject::callSafe()}
+     *
+     * @return void
+     */
+    public function testCreateAndSet(): void
+    {
+        // * * * Обычные функции
+
+        // создание для обычной функции
+        $functionObject = new FunctionAsPropertyObject('array_sum');
+        self::assertEquals(5, $functionObject->call([2, 3]));
+        self::assertEquals(5, $functionObject([2, 3]));
+
+        // защищенный вызов, array_sum() поддерживает только один аргумент, вызов с 2 аргументами приведет к ошибке, ошибка будет проигнорирована
+        $functionObject = new FunctionAsPropertyObject('array_sum');
+        self::assertEquals(4, $functionObject->call([1, 3], 123123));
+        $functionObject = new FunctionAsPropertyObject('array_sum', true);
+        self::assertEquals(7, $functionObject->call([3, 4], 123123));
+
+        // вызов без защищенного режима приведет к ошибке, так как array_sum() может принять только 1 аргумент
+        $functionObject = new FunctionAsPropertyObject('array_sum', false);
+        ExceptionTools::wasCalledWithException(
+            [$functionObject, 'call'],
+            [[3, 4], 123123],
+            \ArgumentCountError::class
+        );
+        // принудительный вызов в защищенном режиме
+        self::assertEquals(7, $functionObject->callSafe([3, 4], 123123));
+
+
+        // * * * Языковые конструкции
+
+        // создание и вызов для языковой конструкции
+        $functionObject = new FunctionAsPropertyObject('array');
+        self::assertEquals([2, 4, 6], $functionObject->call(2, 4, 6));
+
+        // даже если выключен защищенный режим, он будет применен, так как array() не функция
+        $functionObject = new FunctionAsPropertyObject('array', false);
+        self::assertEquals([2, 4, 6], $functionObject->call(2, 4, 6));
+    }
+
+    /**
+     * Test for {@covers FunctionAsPropertyObject::getOrCreate()}
+     *
+     * @return void
+     */
+    public function testGetOrCreate(): void
+    {
+        $functionObject = FunctionAsPropertyObject::getOrCreate('array_sum');
+        self::assertTrue($functionObject instanceof FunctionAsPropertyObject);
+
+        $newFunctionObject = FunctionAsPropertyObject::getOrCreate($functionObject);
+        self::assertTrue($functionObject === $newFunctionObject);
+    }
+}

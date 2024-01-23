b37e23f71f3119e3e54eace487dea479f26e25af
Konstantin Marataev <29762625+dracul-aid@users.noreply.github.com>
2024-01-24 00:10:45 +0400

    Добавлен хэлпер для работы с массивами
    Добавлен хэлпер, для работы с массивами, см Arrays\ArrayHelper

---
+++ src/Arrays/ArrayHelper.php
@@ -0,0 +1,183 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of PhpTools - https://github.com/dracul-aid/PhpTools
+ *
+ * (c) Konstantin Marataev <dracul.aid@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DraculAid\PhpTools\Arrays;
+
+use DraculAid\PhpTools\Arrays\Objects\Interfaces\ArrayInterface;
+use DraculAid\PhpTools\Classes\ClassTools;
+
+/**
+ * Набор функций для работы с массива
+ * (Также работает с массивоподобными объектами, см {@see ArrayInterface})
+ *
+ * - См также:
+ * <br>{@see ClassTools::isAsArray()} Проверит, похож ли объект на массив
+ *
+ * - Оглавление:
+ * <br>--- Проверки
+ * <br>{@see ArrayHelper::isAsArray()} Проверит, что значение является массивом или объектом, похожим на массив
+ * <br>{@see ArrayHelper::keyExist()} Проверит, есть ли в массиве или объекте схожем с массивом значение, по указанному ключу
+ ** <br>--- Работа с данными
+ * <br>{@see ArrayHelper::getNewIndex()} Вернет числовой индекс, который будет присвоен новому элементу массива
+ * <br>{@see ArrayHelper::countSafe()} Вернет кол-во элементов в массиве, если посчитать невозможно - вернет "значение по умолчанию"
+ * <br>{@see ArrayHelper::setInPositionAndMoveOldValues()} Вставит новые значения в массив (начиная с указанной позиции)
+ */
+final class ArrayHelper
+{
+    /**
+     * Проверит, что переданное значение массив, или объект похожий на массив
+     *
+     * @see ClassTools::isAsArray() Проверит, похож ли класс или объект на массив
+     * @see ArrayInterface Интерфейс для объектов, схожих с массивами
+     *
+     * @param   mixed   $asArray     Значение для проверки
+     * @param   bool    $countable   Должно ли переданное значение корректно отрабатываться функцией {@see count()}
+     *
+     * @return  bool
+     *
+     * @todo PHP8 Типизация для аргументов
+     */
+    public static function isAsArray($asArray, bool $countable = true): bool
+    {
+        if (is_array($asArray)) return true;
+
+        if (!is_object($asArray)) return false;
+
+        if ($asArray instanceof \Traversable === false || $asArray instanceof \ArrayAccess === false) return false;
+
+        if ($countable) return $asArray instanceof \Countable;
+        return true;
+    }
+
+    /**
+     * Защищено вызовет {@see count()} для значения, если значение не поддерживает подсчет кол-ва элементов, вернет
+     * заранее установленное значение
+     *
+     * @param   mixed   $asArray   Значение для вызова count()
+     * @param   int     $ifError   Что вернет функция, если подсчет невозможен
+     *
+     * @return int
+     */
+    public static function countSafe($asArray, int $ifError = 0): int
+    {
+        if (is_array($asArray) || $asArray instanceof \Countable) return count($asArray);
+
+        return $ifError;
+    }
+
+    /**
+     * Проверит, существует ли в массиве ключ
+     * (Также работает с массивоподобными объектами)
+     *
+     * @param   array|(\ArrayAccess&\Traversable)   $asArray               Массив или массивоподобный объект
+     * @param   mixed                               $key                   Ключ
+     * @param   bool                                $realSearchForObject   Как будет проверяться объекты:
+     *                                                                     <br>FALSE: с помощью isset()
+     *                                                                     <br>TRUE: с помощью перебора элементов (может серьезно замедлить проверку)
+     *
+     * @return  bool
+     */
+    public static function keyExist($asArray, $key, bool $realSearchForObject = false): bool
+    {
+        // @todo PHP8.2 условие с !ArrayHelper::isAsArray теряет смысл
+        if (!self::isAsArray($asArray, false)) throw new \TypeError('$array can be array or array-object, call is a ' .gettype($asArray));
+
+        if (is_array($asArray)) return array_key_exists($key, $asArray);
+
+        if (self::countSafe($asArray, 1) === 0) return false;
+        elseif (isset($asArray[$key])) return true;
+        elseif (!$realSearchForObject) return false;
+
+        foreach ($asArray as $index => $value)
+        {
+            if ($key == $index) return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Вернет числовой индекс, который будет присвоен новому элементу массива
+     *
+     * Может вернуть 0 (для пустого массива), значение функции count(), в случае, если в массиве есть значения, или иное
+     * число, если индекс с указанным номером уже существует
+     *
+     * @param   array|(\Countable&\ArrayAccess&\Traversable)   $asArray
+     *
+     * @return  int
+     *
+     * @todo PHP8 Типизация для аргументов
+     * @todo PHP8.2 Типизация для аргументов
+     */
+    public static function getNewIndex($asArray): int
+    {
+        // @todo PHP8.2 условие с !ArrayHelper::isAsArray теряет смысл
+        if (!ArrayHelper::isAsArray($asArray)) throw new \TypeError('$array can be array or array-object, call is a ' .gettype($asArray));
+
+        if (count($asArray) === 0) return 0;
+
+        $resultIndex = count($asArray);
+        if (self::keyExist($asArray, $resultIndex))
+        {
+            while (self::keyExist($asArray, $resultIndex));
+        }
+
+        return $resultIndex;
+    }
+
+    /**
+     * Вставит в указанную позицию массива один или несколько элементов (сдвинув остальные элементы массива)
+     *
+     * - ВНИМАНИЕ
+     * <br>(!) Сдвигаемые элементы, поменяют свои числовые индексы
+     * <br>(!) Работает только с массивами, не поддерживает объекты похоже на массивы. Это связано с тем, что массивоподобные
+     * структуры могут иметь сложную логику связи индекса и значения, из-за чего "сдвиг" может привести к непредвиденным последствиям
+     *
+     * @param   array     $array      Массив, или объект похожий на массив
+     * @param   int       $position   Позиция для вставки (если отрицательное число, позиция будет найден с конца массива)
+     * @param   mixed[]   $values     Значения для вставки
+     *
+     * @return  array
+     */
+    public static function setInPositionAndMoveOldValues(array $array, int $position, ... $values): array
+    {
+        if (count($array) === 0) return $values;
+        if (count($values) === 0) return $array;
+
+        if ($position < 0) $position = count($array) + $position;
+
+        // * * * Если длина массива меньше указанной позиции - вставим в конец массива
+
+        if (count($array) <= $position)
+        {
+            foreach ($values as $value) $array[] = $value;
+            return $array;
+        }
+
+        // * * * Если вставка будет проводиться внутрь существующего массива
+
+        // срез массива "до" и "после" позиции
+        $arrayAfter = array_slice($array, $position);
+        // срез массива "до" позиции
+        $arrayResult = array_slice($array, 0, $position);
+
+        // новые значения
+        foreach ($values as $value) $arrayResult[] = $value;
+        // срез массива "после" позиции
+        foreach ($arrayAfter as $key => $value)
+        {
+            if (is_int($key) || (string)(int)$key === $key) $arrayResult[] = $value;
+            else $arrayResult[$key] = $value;
+        }
+
+        return $arrayResult;
+    }
+}
---
+++ tests/Arrays/ArrayHelperTest.php
@@ -0,0 +1,158 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of PhpTools - https://github.com/dracul-aid/PhpTools
+ *
+ * (c) Konstantin Marataev <dracul.aid@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DraculAid\PhpTools\tests\Arrays;
+
+use PHPUnit\Framework\TestCase;
+use DraculAid\PhpTools\Arrays\ArrayHelper;
+
+/**
+ * Test for {@coversDefaultClass ArrayHelper}
+ *
+ * @run php tests/run.php tests/Arrays/ArrayHelperTest.php
+ */
+class ArrayHelperTest extends TestCase
+{
+    /**
+     * Test for {@covers ArrayHelper::isAsArray()}
+     *
+     * @return void
+     */
+    public function testIsAsArray(): void
+    {
+        self::assertFalse(ArrayHelper::isAsArray(111));
+        self::assertFalse(ArrayHelper::isAsArray(111.222));
+        self::assertFalse(ArrayHelper::isAsArray('ABC'));
+        self::assertFalse(ArrayHelper::isAsArray(null));
+        self::assertFalse(ArrayHelper::isAsArray(true));
+        self::assertFalse(ArrayHelper::isAsArray(new \stdClass()));
+
+        self::assertTrue(ArrayHelper::isAsArray([]));
+        self::assertTrue(ArrayHelper::isAsArray(new \ArrayObject()));
+    }
+
+    /**
+     * Test for {@covers ArrayHelper::countSafe()}
+     *
+     * @return void
+     */
+    public function testCountSafe(): void
+    {
+        self::assertEquals(0, ArrayHelper::countSafe(111));
+        self::assertEquals(12, ArrayHelper::countSafe(111, 12));
+        self::assertEquals(0, ArrayHelper::countSafe([], 12));
+        self::assertEquals(4, ArrayHelper::countSafe([1, 3, 5, 7], 12));
+        self::assertEquals(0, ArrayHelper::countSafe(new \ArrayObject([]), 12));
+        self::assertEquals(4, ArrayHelper::countSafe(new \ArrayObject([1, 3, 5, 7]), 12));
+    }
+
+    /**
+     * Test for {@covers ArrayHelper::keyExist()}
+     *
+     * @return void
+     */
+    public function testKeyExist(): void
+    {
+        self::assertFalse(ArrayHelper::keyExist([], 'test-key'));
+        self::assertTrue(ArrayHelper::keyExist(['test-key' => null], 'test-key'));
+        self::assertTrue(ArrayHelper::keyExist(['test-key' => 111], 'test-key'));
+
+        self::assertFalse(ArrayHelper::keyExist(new \ArrayObject(), 'test-key'));
+        self::assertFalse(ArrayHelper::keyExist(new \ArrayObject(['test-key' => null]), 'test-key'));
+        self::assertTrue(ArrayHelper::keyExist(new \ArrayObject(['test-key' => 111]), 'test-key'));
+
+        self::assertFalse(ArrayHelper::keyExist(new \ArrayObject(), 'test-key', false));
+        self::assertFalse(ArrayHelper::keyExist(new \ArrayObject(['test-key' => null]), 'test-key', false));
+        self::assertTrue(ArrayHelper::keyExist(new \ArrayObject(['test-key' => 111]), 'test-key', false));
+
+        self::assertFalse(ArrayHelper::keyExist(new \ArrayObject(), 'test-key', true));
+        self::assertTrue(ArrayHelper::keyExist(new \ArrayObject(['test-key' => null]), 'test-key', true));
+        self::assertTrue(ArrayHelper::keyExist(new \ArrayObject(['test-key' => 111]), 'test-key', true));
+    }
+
+    /**
+     * Test for {@covers ArrayHelper::getNewIndex()}
+     *
+     * @return void
+     */
+    public function testGetNewIndex(): void
+    {
+        self::assertTrue(true);
+        return;
+
+        $arr = [];
+        self::assertEquals(0, ArrayHelper::getNewIndex($arr));
+
+        $arr = ['A'];
+        self::assertEquals(1, ArrayHelper::getNewIndex($arr));
+
+        $arr = [1 => 'A'];
+        self::assertEquals(2, ArrayHelper::getNewIndex($arr));
+    }
+
+    /**
+     * Test for {@covers ArrayHelper::setInPositionAndMoveOldValues()}
+     *
+     * @return void
+     */
+    public function testSetInPositionAndMoveOldValues(): void
+    {
+        // массив в который производится вставка пуст - вернем вставляемые данные
+        self::assertEquals(
+            [0=>'a', 1=>'b', 2=>'c', 3=>'d', 4=>'e'],
+            ArrayHelper::setInPositionAndMoveOldValues(
+                [],
+                0,
+                'a', 'b', 'c', 'd', 'e'
+            )
+        );
+
+        // вставляемых данных нет - вернем изначальный массив как есть
+        self::assertEquals(
+            [0=>'a', 1=>'b', 2=>'c', 3=>'d', 4=>'e'],
+            ArrayHelper::setInPositionAndMoveOldValues(
+                [0=>'a', 1=>'b', 2=>'c', 3=>'d', 4=>'e'],
+                0,
+                ... []
+            )
+        );
+
+        // позиция для вставки превышает кол-во элементов в изначальном массиве - вставим в конец массива
+        self::assertEquals(
+            [0=>'a', 1=>'b', 2=>'c', 3=>'d', 4=>'e'],
+            ArrayHelper::setInPositionAndMoveOldValues(
+                    [0=>'a', 1=>'b'],
+                    100,
+                    'c', 'd', 'e'
+            )
+        );
+
+        // вставляем данные в центр существующего массива
+        self::assertEquals(
+            [0=>'a', 1=>'b', 2=>'x', 3=>'y', 4=>'z', 5=>'c', 6=>'d', 7=>'e'],
+            ArrayHelper::setInPositionAndMoveOldValues(
+                [0=>'a', 1=>'b', 2=>'c', 3=>'d', 4=>'e'],
+                2,
+                'x', 'y', 'z'
+            )
+        );
+
+        // вставляем данные в центр существующего массива с Отрицательным индексом
+        self::assertEquals(
+            [0=>'a', 1=>'b', 2=>'x', 3=>'y', 4=>'z', 5=>'c', 6=>'d', 7=>'e'],
+            ArrayHelper::setInPositionAndMoveOldValues(
+                [0=>'a', 1=>'b', 2=>'c', 3=>'d', 4=>'e'],
+                -3,
+                'x', 'y', 'z'
+            )
+        );
+    }
+}

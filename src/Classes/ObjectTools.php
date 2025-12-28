7205d323090501e4eacb0ace93a3c1865dde1289
Konstantin Marataev <dracul.aid@gmail.com>
2025-12-28 15:55:42 +0400

    Добавлен метод `Classes\ObjectTools::getStringNewInstance()` Позволяет создавать строки создания объектов, т.е. `new ClassName()`

--- src/Classes/ObjectTools.php
+++ src/Classes/ObjectTools.php
@@ -19,6 +19,7 @@ use DraculAid\PhpTools\tests\Classes\ObjectToolsTest;
  * Оглавление:
  * <br>{@see ObjectTools::propertiesFor()} Генератор для перебора свойств объекта (именно свойства, даже если реализован {@see \Traversable})
  * <br>{@see ObjectTools::toArray()} Вернет массив свойств объекта (именно свойства, даже если реализован {@see \Traversable})
+ * <br>{@see ObjectTools::getStringNewInstance()} Вернет строку создания нового экземпляра класса, используется, когда нужно динамически создать PHP код с созданием объекта
  *
  * Test cases for class {@see ObjectToolsTest}
  */
@@ -69,5 +70,78 @@ final class ObjectTools
     public static function toArray(object $object, bool $intKey = false, null|int $filter = \ReflectionProperty::IS_PUBLIC): array
     {
         return iterator_to_array(self::propertiesFor($object, $intKey, $filter));
+    }
+
+    /**
+     * Вернет строку создания нового экземпляра класса, используется, когда нужно динамически создать PHP код с созданием объекта
+     *
+     * (!) Вставка аргументов в строку происходит с помощью функции {@see var_export()}
+     *
+     * Пример аргументов ввиде списка
+     * ```php
+     * $arguments = ['first', 222, true];
+     * echo ObjectTools::getStringNewInstance(SomeClass::class, $arguments);
+     * // Выведет:
+     * // SomeClass::new('first', 222, true)
+     * ```
+     *
+     * Пример аргументов ввиде ассоциативного массива
+     * ```php
+     * $arguments = ['name' => 'first', 'count' => 222, 'isActive' => true];
+     * echo ObjectTools::getStringNewInstance(SomeClass::class, $arguments);
+     * // Выведет:
+     * // SomeClass::new(name: 'first', count: 222, isActive: true)
+     * ```
+     *
+     * Пример аргументов ввиде смешанного массива
+     * ```php
+     * $arguments = ['name' => 'first', 222, 'isActive' => true, false];
+     * echo ObjectTools::getStringNewInstance(SomeClass::class, $arguments);
+     * // Выведет:
+     * // SomeClass::new(name: 'first', 222, isActive: true, false)
+     * ```
+     *
+     * @param   string     $class                  Полное имя класса
+     * @param   iterable   $constructorArguments   Массив с данными для конструктора.
+     *                                             <br>* Если передан список - аргументы будут переданы в порядке, указанном в массиве.
+     *                                             <br>* Если передан ассоциативный массив cо ключами-строками - аргументы будут переданы по именам.
+     *                                             <br>* Если передан ассоциативный массив cо строковыми и числовыми ключами - будет выброшено исключение
+     * @param   bool         $argumentsAsList      TRUE - если аргументы переданы списком, FALSE - если функция сама определит, как были переданы аргументы
+     *
+     * @return  string    Вернет готовый к использованию PHP код создания нового экземпляра класса
+     * @throws \InvalidArgumentException Если аргументы конструктора переданы ввиде ассоциативного массива, в котором есть не строковые ключи
+     */
+    public static function getStringNewInstance(string $class, iterable $constructorArguments, bool $argumentsAsList = false): string
+    {
+        /** Аргументы для создания PHP кода создания объекта */
+        $argumentsArray = [];
+
+        $asList = true;
+        $argumentPosition = -1;
+        foreach ($constructorArguments as $index => $item)
+        {
+            if (is_object($index) && method_exists($index, '__toString'))
+            {
+                $index = (string)$index;
+            }
+
+            if ($argumentsAsList || (is_int($index) && $asList))
+            {
+                $argumentsArray[] = var_export($item, true);
+            }
+            elseif (is_string($index))
+            {
+                $asList = false;
+                $argumentsArray[] = "{$index}: " . var_export($item, true);
+            }
+            else
+            {
+                throw new \InvalidArgumentException("Arguments for constructor must be an associative array with string keys or a list with integer keys, found argument #{$argumentPosition} with type: " . gettype($index));
+            }
+
+            $argumentPosition++;
+        }
+
+        return "new \\{$class}(" . implode(', ', $argumentsArray) . ')';
     }
 }
--- tests/Classes/ObjectToolsTest.php
+++ tests/Classes/ObjectToolsTest.php
@@ -12,6 +12,7 @@
 namespace DraculAid\PhpTools\tests\Classes;
 
 use DraculAid\PhpTools\Classes\ObjectTools;
+use DraculAid\PhpTools\ExceptionTools\ExceptionTools;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -21,6 +22,71 @@ use PHPUnit\Framework\TestCase;
  */
 class ObjectToolsTest extends TestCase
 {
+    /**
+     * Test for {@see ObjectTools::getStringNewInstance()}
+     */
+    public function testGetStringNewInstance(): void
+    {
+        // * * * Пустые конструкторы
+
+        self::assertEquals(
+            'new \stdClass()',
+            ObjectTools::getStringNewInstance(\stdClass::class, [])
+        );
+
+        self::assertEquals(
+            'new \stdClass()',
+            ObjectTools::getStringNewInstance(\stdClass::class, new \ArrayObject([]))
+        );
+
+        // * * * Конструкторы, с параметрами переданными ввиде списка
+
+        self::assertEquals(
+            "new \stdClass('first', 222, true, false, NULL)",
+            ObjectTools::getStringNewInstance(\stdClass::class, ["first", 222, true, false, null])
+        );
+
+        self::assertEquals(
+            "new \stdClass('first', 222, true, false, NULL)",
+            ObjectTools::getStringNewInstance(\stdClass::class, ['1n' => "first", 222, true, false, '2n' => null], true)
+        );
+
+        self::assertEquals(
+            "new \stdClass('first', 222, true, false, NULL)",
+            ObjectTools::getStringNewInstance(\stdClass::class, ['x1' => "first", 'x2'=> 222, 'x3' => true, 'x4' => false, 'x5' => null], true)
+        );
+
+        self::assertEquals(
+            'new \stdClass(1, 2, 3)',
+            ObjectTools::getStringNewInstance(\stdClass::class, new \ArrayObject([1, 2, 3]))
+        );
+
+        // * * * Конструкторы, с параметрами, переданными ввиде именованных аргументов
+
+        self::assertEquals(
+            "new \stdClass(x1: 'first', x2: 222, x3: true, x4: false, x5: NULL)",
+            ObjectTools::getStringNewInstance(\stdClass::class, ['x1' => "first", 'x2'=> 222, 'x3' => true, 'x4' => false, 'x5' => null])
+        );
+
+        self::assertEquals(
+            "new \stdClass(x1: 'first', x2: 222, x3: true, x4: false, x5: NULL)",
+            ObjectTools::getStringNewInstance(\stdClass::class, ['x1' => "first", 'x2'=> 222, 'x3' => true, 'x4' => false, 'x5' => null], false)
+        );
+
+        // * * * Смешанные параметры - будет выброшена ошибка, перехватим ее
+
+        self::assertTrue(
+            ExceptionTools::wasCalledWithException([ObjectTools::class, 'getStringNewInstance'], [\stdClass::class, ['1n' => "first", 222, true, false, '2n' => null]], \InvalidArgumentException::class)
+        );
+
+        self::assertTrue(
+            ExceptionTools::wasCalledWithException([ObjectTools::class, 'getStringNewInstance'], [\stdClass::class, ['1n' => "first", 222, true, false, '2n' => null], false], \InvalidArgumentException::class)
+        );
+
+        // * * * TODO реализовать следующие кейсы
+        // 1. Передача аргументов "массивом" с ключами-объектами, должно падать (если объект не имеет метод __toString())
+    }
+
     /**
      * Test for {@see ObjectTools::propertiesFor()}
      * Test for {@see ObjectTools::toArray()}
